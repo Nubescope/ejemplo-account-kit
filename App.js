055f7fdd3ccbc741c518e7dcb2792544124aeb4b
Juan Pablo Garcia <juanpablogarcia@gmail.com>
2018-09-28 10:32:24 -0300

    Integrate sample with backend services

--- App.js
+++ App.js
@@ -1,27 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, Button, Text } from 'react-native'
+import RNAccountKit from 'react-native-facebook-account-kit'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-})
+const API_URL = 'http://localhost:3000'
 
-type Props = {}
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    jwt: null,
+    me: null,
+  }
+
+  componentDidMount() {
+    RNAccountKit.configure({
+      responseType: 'code',
+      initialPhoneCountryPrefix: '+54',
+      defaultCountry: 'AR',
+    })
+  }
+
+  handleLoginButtonPress = async () => {
+    try {
+      const payload = await RNAccountKit.loginWithPhone()
+
+      if (!payload) {
+        return
+      }
+
+      const { code } = payload
+
+      await this.getJWT(code)
+    } catch (err) {
+      alert('Error en autenticación con Facebook.')
+    }
+  }
+
+  handleLogoutPress = () => this.setState({ jwt: null, me: null })
+
+  getJWT = async code => {
+    const url = `${API_URL}/auth?code=${code}`
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (!res.ok) {
+      alert('No se pudo obtener el JWT')
+      return
+    }
+
+    const { jwt } = await res.json()
+    this.setState({ jwt })
+  }
+
+  handleGetMePress = async () => {
+    const url = `${API_URL}/me`
+    const { jwt } = this.state
+
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (res.status === 403) {
+      alert('Usuario no autorizado')
+      return
+    } else if (!res.ok) {
+      alert('No se pudo obtener el perfil')
+      return
+    }
+
+    const me = await res.json()
+    this.setState({ me })
+  }
+
   render() {
+    const { jwt, me } = this.state
+
+    const authenticated = !!jwt
+    const phone = !!me && me.phone
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {!authenticated && <Button title="Login" onPress={this.handleLoginButtonPress} />}
+
+        {authenticated && (
+          <View style={styles.container}>
+            <Text style={styles.title}>Bienvenido!</Text>
+
+            <Text style={styles.jwt}>{jwt}</Text>
+
+            {phone && <Text style={styles.phone}>Teléfono: {phone.number}</Text>}
+
+            <Button title="Obtener Perfil" onPress={this.handleGetMePress} />
+            <Button title="Salir" onPress={this.handleLogoutPress} />
+          </View>
+        )}
       </View>
     )
   }
@@ -32,16 +111,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 20,
   },
-  welcome: {
+
+  title: {
+    fontWeight: 'bold',
+    padding: 20,
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  phone: {
+    padding: 20,
+    fontSize: 14,
+  },
+
+  phone: {
+    padding: 20,
+    fontSize: 14,
   },
 })
